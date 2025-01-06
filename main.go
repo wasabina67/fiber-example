4e963b5b03f0d3d00ef540dde8122623a536aa92
wasabina67 <wasabina67@gmail.com>
2025-01-06 17:16:37 +0900

    Update main.go

--- main.go
+++ main.go
@@ -7,15 +7,11 @@ import (
 )
 
 func main() {
-    // Initialize a new Fiber app
     app := fiber.New()
 
-    // Define a route for the GET method on the root path '/'
     app.Get("/", func(c fiber.Ctx) error {
-        // Send a string response to the client
         return c.SendString("Hello, World 👋!")
     })
 
-    // Start the server on port 3000
     log.Fatal(app.Listen(":3000"))
 }
