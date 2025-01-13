8ffcbde88e52291b2ac0186fb141f7d4f12afc2e
wasabina67 <wasabina67@gmail.com>
2025-01-13 13:17:19 +0900

    gofmt -w main.go

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-    "github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3"
 )
 
 func main() {
-    app := fiber.New()
+	app := fiber.New()
 
-    app.Get("/", func(c fiber.Ctx) error {
-        return c.SendString("Hello, World 👋!")
-    })
+	app.Get("/", func(c fiber.Ctx) error {
+		return c.SendString("Hello, World 👋!")
+	})
 
-    app.Listen(":3000")
+	app.Listen(":3000")
 }
